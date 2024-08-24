faca37807381790be5f4ff3d3208b9eaec706377
Luis Eduardo <banedycraft@gmail.com>
2024-08-24 15:45:26 -0300

    first commit

---
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/OwnerRepository.java
@@ -0,0 +1,13 @@
+package com.packt.cardatabase.domain;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+	Optional<Owner> findByFirstname(String firstName);
+
+}
